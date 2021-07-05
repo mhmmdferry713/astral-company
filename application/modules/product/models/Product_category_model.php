b1204e5c7e20b502a8e9b0a813154777e4a1ed73
mhmmdferry713 <mhmmdferry713@webmail.umm.ac.id>
2021-07-05 22:36:54 +0800

    uas-final-v38

--- application/modules/product/models/Product_category_model.php
+++ application/modules/product/models/Product_category_model.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class ProductCategoryModel extends CI_Model
+class Product_category_model extends CI_Model
 {
   private $_table = 'product_category';
   private $_tableView = '';
