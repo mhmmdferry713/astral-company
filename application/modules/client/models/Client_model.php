a67386dbb2de0682b4fb4cd85c05235f999a85ea
mhmmdferry713 <mhmmdferry713@webmail.umm.ac.id>
2021-07-05 22:24:37 +0800

    uas-final-v33

--- application/modules/client/models/Client_model.php
+++ application/modules/client/models/Client_model.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class ClientModel extends CI_Model
+class Client_model extends CI_Model
 {
   private $_table = 'client';
   private $_tableView = '';
