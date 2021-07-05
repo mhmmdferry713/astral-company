8801dc62e822c00ac51ec30a03f3a868686dca18
mhmmdferry713 <mhmmdferry713@webmail.umm.ac.id>
2021-07-05 18:31:13 +0800

    uas-final-v21

--- application/config/database.php
+++ application/config/database.php
@@ -97,14 +97,14 @@ $query_builder = TRUE;
 // 	'save_queries' => TRUE
 // );
 
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$url = parse_url(getenv("mysql://b778cbf9ea1e62:bd5d7924@us-cdbr-east-04.cleardb.com/heroku_d15c663669c91ef?reconnect=true"));
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => $url["host"],
-	'username' => $url["user"],
-	'password' => $url["pass"],
-	'database' => substr($url["path"], 1),
+	'hostname' => 'us-cdbr-east-04.cleardb.com',
+	'username' => 'b778cbf9ea1e62',
+	'password' => 'bd5d7924',
+	'database' => 'heroku_d15c663669c91ef',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
